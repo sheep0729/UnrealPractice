2a7fa90c1242222bc3110d0b3108352bc3265ae3
Yang Dong <yangdong0729@outlook.com>
2023-10-25 13:45:22 +0800

    One Course Solution for Material - Dynamic Material Instance

---
+++ Source/Practice/Utility/UtilityDemo.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UtilityDemo.generated.h"
+
+
+
+UCLASS()
+class PRACTICE_API UUtilityDemo : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	
+	UFUNCTION(BlueprintCallable, Category = "UtilityDemo|Regex")
+	static void RegexDemo();
+};
