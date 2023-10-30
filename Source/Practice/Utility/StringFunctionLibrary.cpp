defc2f3ce7787581f0fd802fefa102fa4506c44d
Yang Dong <yangdong0729@outlook.com>
2023-10-30 15:57:25 +0800

    优化中文显示效果：添加中文分词规则

---
+++ Source/Practice/Utility/StringFunctionLibrary.cpp
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StringFunctionLibrary.h"
+#include <string>
+
+inline bool UStringFunctionLibrary::LessEqual_StringString(FString A, FString B)
+{
+	// return std::basic_string(*A) <= std::basic_string(*B);
+	return A.Compare(B, ESearchCase::CaseSensitive) <= 0;
+}
+
+bool UStringFunctionLibrary::Equal_StringString(FString A, FString B)
+{
+	return A.Compare(B, ESearchCase::CaseSensitive) == 0;
+}
---
+++ Source/Practice/Utility/StringFunctionLibrary.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "StringFunctionLibrary.generated.h"
+
+UCLASS()
+class PRACTICE_API UStringFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "string <= string", CompactNodeTitle = "<=", Keywords = "<= less"), Category="Math|String")
+	static bool LessEqual_StringString(FString A, FString B);
+
+	UFUNCTION(BlueprintPure, meta=(DisplayName = "string == string", CompactNodeTitle = "==", Keywords = "== equal"), Category="Math|String")
+	static bool Equal_StringString(FString A, FString B);
+};
