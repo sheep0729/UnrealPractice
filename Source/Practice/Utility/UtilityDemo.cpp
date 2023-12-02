f3c94ea6d5432d8760fef57e083965acae1131e7
Yang Dong <yangdong0729@outlook.com>
2023-12-02 17:25:41 +0800

    Regex

--- Source/Practice/Utility/UtilityDemo.cpp
+++ Source/Practice/Utility/UtilityDemo.cpp
@@ -5,5 +5,11 @@
 
 void UUtilityDemo::RegexDemo()
 {
-	
+	const FString TextStr("ABCDEFGHIJKLMN");
+	const FRegexPattern TestPattern(TEXT("C.+H"));
+	if (FRegexMatcher TestMatcher(TestPattern, TextStr); TestMatcher.FindNext())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("找到匹配内容 [%d, %d)"),
+		       TestMatcher.GetMatchBeginning(), TestMatcher.GetMatchEnding());
+	}
 }
