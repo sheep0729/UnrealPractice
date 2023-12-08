21a189f27014b3691665ce55df5ed792efda26f3
Yang Dong <yangdong0729@outlook.com>
2023-12-08 12:56:28 +0800

    Image

--- Source/Practice/Utility/UtilityDemo.cpp
+++ Source/Practice/Utility/UtilityDemo.cpp
@@ -3,8 +3,11 @@
 
 #include "UtilityDemo.h"
 
+#include "IImageWrapper.h"
+#include "IImageWrapperModule.h"
 #include "XmlFile.h"
 #include "XmlNode.h"
+#include "HAL/FileManagerGeneric.h"
 
 void UUtilityDemo::RegexDemo()
 {
@@ -88,3 +91,120 @@ void UUtilityDemo::GConfigDemo()
 	UE_LOG(LogTemp, Display, TEXT("Name = %s"), *Result);
 }
 
+void UUtilityDemo::ConvertImageDemo()
+{
+	const FString ImageFilePathNoExt = FPaths::ProjectDir() / "Asset" / "ewe_1f4112";
+	CovertPNG2JPG(ImageFilePathNoExt + ".png", ImageFilePathNoExt + ".jpg");
+}
+
+bool UUtilityDemo::CovertPNG2JPG(const FString& SourceName, const FString& TargetName)
+{
+	check(SourceName.EndsWith(TEXT(".png")) && TargetName.EndsWith(TEXT(".jpg")));
+
+	// TODO 这个函数的实现方式不是 IImageWrapperModule 注释中推荐的，是比较底层的方式
+	IImageWrapperModule& ImageWrapperModule = FModuleManager::LoadModuleChecked<IImageWrapperModule>(FName("ImageWrapper"));
+
+	const TSharedPtr<IImageWrapper> SourceImageWrapper = ImageWrapperModule.CreateImageWrapper(EImageFormat::PNG);
+	const TSharedPtr<IImageWrapper> TargetImageWrapper = ImageWrapperModule.CreateImageWrapper(EImageFormat::JPEG);
+
+	TArray<uint8> SourceImageData;
+	TArray<uint8> TargetImageData;
+
+	if (!FPlatformFileManager::Get().GetPlatformFile().FileExists(*SourceName))
+	{
+		// 文件不存在
+		return false;
+	}
+
+	if (!FFileHelper::LoadFileToArray(SourceImageData, *SourceName))
+	{
+		// 文件读取失败
+		return false;
+	}
+
+	if (SourceImageWrapper.IsValid() && SourceImageWrapper->SetCompressed(SourceImageData.GetData(), SourceImageData.Num()))
+	{
+		if (TArray<uint8> UncompressedRGBA; SourceImageWrapper->GetRaw(ERGBFormat::RGBA, 8, UncompressedRGBA))
+		{
+			const int32 Height = SourceImageWrapper->GetHeight();
+			const int32 Width = SourceImageWrapper->GetWidth();
+
+			if (TargetImageWrapper->SetRaw(UncompressedRGBA.GetData(), UncompressedRGBA.Num(), Width, Height, ERGBFormat::RGBA, 8))
+			{
+				TargetImageData = TargetImageWrapper->GetCompressed();
+				if (!FFileManagerGeneric::Get().DirectoryExists(*TargetName))
+				{
+					FFileManagerGeneric::Get().MakeDirectory(*FPaths::GetPath(TargetName), true);
+				}
+				return FFileHelper::SaveArrayToFile(TargetImageData, *TargetName);
+			}
+		}
+	}
+	return false;
+}
+
+
+TSharedPtr<IImageWrapper> UUtilityDemo::GetImageWrapperByExtension(const FString path)
+{
+	IImageWrapperModule& module = FModuleManager::LoadModuleChecked<IImageWrapperModule>(FName("ImageWrapper"));
+	if (path.EndsWith(".png"))
+	{
+		return module.CreateImageWrapper(EImageFormat::PNG);
+	}
+	if (path.EndsWith(".jpg"))
+	{
+		return module.CreateImageWrapper(EImageFormat::JPEG);
+	}
+
+	return nullptr;
+}
+
+
+UTexture2D* UUtilityDemo::LoadTexture2DFromBytesAndExtension(
+	const FString& ImagePath,
+	const uint8* InCompressedData,
+	const int32 InCompressedSize,
+	int32& OutWidth,
+	int32& OutHeight
+)
+{
+	UTexture2D* Texture = nullptr;
+	const TSharedPtr<IImageWrapper> ImageWrapper = GetImageWrapperByExtension(ImagePath);
+	if (ImageWrapper.IsValid() && ImageWrapper->SetCompressed(InCompressedData, InCompressedSize)) // 读取压缩后的图片数据
+	{
+		if (TArray<uint8> UncompressedRGBA; ImageWrapper->GetRaw(ERGBFormat::RGBA, 8, UncompressedRGBA)) // 获取原始图片数据
+		{
+			Texture = UTexture2D::CreateTransient(ImageWrapper->GetWidth(), ImageWrapper->GetHeight(), PF_R8G8B8A8);
+			if (Texture != nullptr)
+			{
+				// 通过内存复制，填充原始 RGB 数据到贴图的数据中
+				OutWidth = ImageWrapper->GetWidth();
+				OutHeight = ImageWrapper->GetHeight();
+
+				void* TextureData = Texture->GetPlatformData()->Mips[0].BulkData.Lock(LOCK_READ_WRITE);
+				FMemory::Memcpy(TextureData, UncompressedRGBA.GetData(), UncompressedRGBA.Num());
+				Texture->GetPlatformData()->Mips[0].BulkData.Unlock();
+				Texture->UpdateResource();
+			}
+		}
+	}
+	return Texture;
+}
+
+UTexture2D* UUtilityDemo::LoadTexture2DFromFilePath(FString& ImagePath, int32& OutWidth, int32& OutHeight)
+{
+	// 文件是否存在
+	if (!FPlatformFileManager::Get().GetPlatformFile().FileExists(*ImagePath))
+	{
+		return nullptr;
+	}
+
+	// 读取文件资源
+	TArray<uint8> CompressedData;
+	if (!FFileHelper::LoadFileToArray(CompressedData, *ImagePath))
+	{
+		return nullptr;
+	}
+
+	return LoadTexture2DFromBytesAndExtension(ImagePath, CompressedData.GetData(), CompressedData.Num(), OutWidth, OutHeight);
+}
--- Source/Practice/Utility/UtilityDemo.h
+++ Source/Practice/Utility/UtilityDemo.h
@@ -7,6 +7,7 @@
 #include "UtilityDemo.generated.h"
 
 
+class IImageWrapper;
 
 UCLASS()
 class PRACTICE_API UUtilityDemo : public UBlueprintFunctionLibrary
@@ -14,7 +15,6 @@ class PRACTICE_API UUtilityDemo : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-	
 	UFUNCTION(BlueprintCallable, Category = "Utility Demo")
 	static void RegexDemo();
 
@@ -29,4 +29,20 @@ public:
 
 	UFUNCTION(BlueprintCallable, Category = "Utility Demo")
 	static void GConfigDemo();
+
+	UFUNCTION(BlueprintCallable, Category = "Utility Demo")
+	static void ConvertImageDemo();
+
+	UFUNCTION(BlueprintCallable, Category = "Utility Demo")
+	static bool CovertPNG2JPG(const FString& SourceName, const FString& TargetName);
+
+	// 从图片直接加载纹理
+	UFUNCTION(BlueprintCallable, Category = "Utility Demo")
+	static UTexture2D* LoadTexture2DFromFilePath(FString& ImagePath, int32& OutWidth, int32& OutHeight);
+
+private:
+	static UTexture2D* LoadTexture2DFromBytesAndExtension(const FString& ImagePath, const uint8* InCompressedData, int32 InCompressedSize, int32& OutWidth,
+	                                                      int32& OutHeight);
+
+	static TSharedPtr<IImageWrapper> GetImageWrapperByExtension(const FString path);
 };
